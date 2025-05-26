e9179ef41c752d02e94a0a94620a03e3be9a0209
luka1sys <dgebuadzeluka2008@gmail.com>
2025-05-26 12:54:38 +0400

    commit

---
+++ day 0129/homework/my-project/src/profile.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main>
+      <h1>about me</h1>
+      <ul>
+        <li>my name is luka </li>
+        <li>my last name is dgebuadze</li>
+        <li>my hoby is hunting</li>
+      </ul>
+    </main>
+  )
+}
+export default App
